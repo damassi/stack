7ef7b0edabecad2c8bdab12532bc5ee8419562b0
Christopher Pappas <damassi.pappas@gmail.com>
2015-10-01 21:22:30 -0700

    Cleanup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,4 @@ import App from 'App';
 import 'styles/normalize.scss';
 import 'styles/global.scss';
 
-ReactDOM.render(
-  <App />
-, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
